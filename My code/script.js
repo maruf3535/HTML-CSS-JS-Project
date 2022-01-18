939f3c6a80556a938ea077d7314853f6d2ce6a52
maruf3535 <97810168+maruf3535@users.noreply.github.com>
2022-01-19 03:00:32 +0600

    Analog Clock

---
+++ My code/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+// (comment) Set a Specific time with next 4 lines code.
+// Like Fri, 26 Sep 2014 18:30:00 GMT
+// var today = new Date();
+// var myToday = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 5, 10, 20);
+// console.log(myToday);
+
